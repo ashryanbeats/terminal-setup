68e61237232855baf82005741b665fbfac3a1c2a
Ash Ryan Arnwine <ashryanarnwine@Ashs-MacBook-Pro.local>
2019-12-05 14:55:30 +0000

    Installing terminal fonts and theme. Fixed n and node modules install.

--- scripts/installnodemodules.sh
+++ scripts/installnodemodules.sh
@@ -6,3 +6,7 @@ nodemodules=("http-server" "jshint" "trash-cli")
 
 echo "Installing global node modules..."
 npm i -g $nodemodules
+
+echo "Installing terminal setup node modules..."
+cd ~/terminal-setup
+npm i
